6a72ed085e4f397b41b4ddac6d0ecba65cb7a7c7
Bojing <75305395+Bojing4313@users.noreply.github.com>
2022-12-06 16:33:52 +0100

    Intergrated offset calculation func

--- circle_detection.py
+++ circle_detection.py
@@ -7,32 +7,35 @@ import matplotlib.pyplot as plt
 IMG_COLOR = None
 DETECTED_CASE = []
 DETECTED_ELECTRODE = []
-OBJECT_LIST = ('Anode', 'Anode_Grab', 'Cathode', 'Cathode_Grab', 'Anode_Spacer', 'Cathode_Spacer', 'Cathode_Case')
+OBJECT_LIST = ('Anode', 'Anode_Grab', 'Cathode', 'Cathode_Grab', 'Anode_Spacer', 'Cathode_Spacer', 'Cathode_Case', 'Suction_Cup')
 
 CONFIG = dict(
-Anode=dict(name='Anode', text_pos = (10, 460), dilate_ksize=(15,15), dilate_iter=1, erode_ksize=(11,11),
+Anode=dict(name='Anode', diam=15, text_pos = (10, 460), dilate_ksize=(15,15), dilate_iter=1, erode_ksize=(11,11),
 erode_iter=1, minDist=100, param1=100, param2=7, minR=92, maxR=93),
 
-Anode_Grab=dict(name='Anode_Grab', text_pos = (10, 460), dilate_ksize=(17,17), dilate_iter=1, erode_ksize=(15,15),
+Anode_Grab=dict(name='Anode_Grab', diam=15, text_pos = (10, 420), dilate_ksize=(17,17), dilate_iter=1, erode_ksize=(15,15),
 erode_iter=1, minDist=100, param1=100, param2=25, minR=125, maxR=135),
 
-Cathode=dict(name='Cathode', text_pos = (10, 460), dilate_ksize=(9,9), dilate_iter=1, erode_ksize=(7,7), 
+Cathode=dict(name='Cathode', diam=14, text_pos = (10, 460), dilate_ksize=(9,9), dilate_iter=1, erode_ksize=(7,7), 
 erode_iter=1, minDist=100, param1=100, param2=9, minR=85, maxR=88),
 
-Cathode_Grab=dict(name='Cathode_Grab', text_pos = (10, 460), dilate_ksize=(17,17), dilate_iter=1, erode_ksize=(17,17), 
+Cathode_Grab=dict(name='Cathode_Grab', diam=14, text_pos = (10, 420), dilate_ksize=(17,17), dilate_iter=1, erode_ksize=(17,17), 
 erode_iter=1, minDist=100, param1=120, param2=15, minR=115, maxR=125),
 
-Anode_Spacer=dict(name='Anode_Spacer', text_pos = (10, 460), dilate_ksize=(19,19), dilate_iter=1, erode_ksize=(15,15), 
+Anode_Spacer=dict(name='Anode_Spacer', diam=15.5, text_pos = (10, 420), dilate_ksize=(19,19), dilate_iter=1, erode_ksize=(15,15), 
 erode_iter=1, minDist=100, param1=120, param2=15, minR=135, maxR=140),
 
-Cathode_Spacer=dict(name='Cathode_Spacer', text_pos = (10, 460), dilate_ksize=(19,19), dilate_iter=1, erode_ksize=(15,15), 
+Cathode_Spacer=dict(name='Cathode_Spacer', diam=15.5, text_pos = (10, 420), dilate_ksize=(19,19), dilate_iter=1, erode_ksize=(15,15), 
 erode_iter=1, minDist=100, param1=120, param2=15, minR=135, maxR=140),
 
-Cathode_Case=dict(name='Cathode_Case', text_pos = (10, 460), dilate_ksize=(19,19), dilate_iter=1, erode_ksize=(15,15), 
+Cathode_Case=dict(name='Cathode_Case', diam=19.3, text_pos = (10, 420), dilate_ksize=(19,19), dilate_iter=1, erode_ksize=(15,15), 
 erode_iter=1, minDist=100, param1=120, param2=15, minR=160, maxR=170),
 
-Reference=dict(name='Reference', text_pos = (10, 440), dilate_ksize=(19,19), dilate_iter=1, erode_ksize=(15,15), 
-erode_iter=1, minDist=100, param1=120, param2=15, minR=13, maxR=16)
+Reference=dict(name='Reference', diam=2, text_pos = (10, 440), dilate_ksize=(19,19), dilate_iter=1, erode_ksize=(15,15), 
+erode_iter=1, minDist=100, param1=120, param2=15, minR=13, maxR=16),
+
+Suction_Cup=dict(name='Suction_Cup', diam=4, text_pos = (10, 420), dilate_ksize=(19,19), dilate_iter=1, erode_ksize=(15,15), 
+erode_iter=1, minDist=100, param1=120, param2=10, minR=54, maxR=60),
 )
 
 def detect_object_center(object_config:dict):
@@ -51,7 +54,11 @@ def detect_object_center(object_config:dict):
     circles = cv.HoughCircles(img_gray, cv.HOUGH_GRADIENT, 1, object_config['minDist'],
                                param1=object_config['param1'], param2=object_config['param2'],
                                minRadius=object_config['minR'], maxRadius=object_config['maxR'])
-
+    (h, w) = IMG_COLOR.shape[:2]
+    imageCenter = (w//2, h//2)
+    cv.line(img=IMG_COLOR, pt1=(imageCenter[0]-5, imageCenter[1]), pt2=(imageCenter[0]+5, imageCenter[1]), color=(0, 255, 0), thickness=1, lineType=8, shift=0)
+    cv.line(img=IMG_COLOR, pt1=(imageCenter[0], imageCenter[1]-5), pt2=(imageCenter[0], imageCenter[1]+5), color=(0, 255, 0), thickness=1, lineType=8, shift=0)
+            
     # Mark the center of the inner circle
     if circles is not None:
         circles = np.uint16(np.around(circles))
@@ -62,12 +69,21 @@ def detect_object_center(object_config:dict):
             # circle outline
             radius = i[2]
             DETECTED_ELECTRODE.append(radius)
+            (h, w) = IMG_COLOR.shape[:2]
+            scale = round(object_config['diam']/(2*radius), 6)
+            offX = round((i[0]-w//2)*scale, 4)
+            offY = round((h//2-i[1])*scale, 4) # Offset in reference to the imgcenter, inversed y axis
+            offSet = (offX, offY) 
+            cv.circle(IMG_COLOR, center, 2, (0,0,255), -1)
             cv.circle(IMG_COLOR, center, radius, (255, 0, 255), 1)
             cv.putText(IMG_COLOR, f"Coordinates of {object_config['name']}: {center[0]} , {center[1]}. Radius: {i[2]}", object_config['text_pos'],
                        cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            cv.putText(IMG_COLOR, f"Offset: {offSet}mm", np.add(object_config['text_pos'], (0,20)),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
     
 def main():
-    print("Choose from following list:\n[1]--> Anode\n[2]--> Anode_Grab\n[3]--> Cathode\n[4]--> Cathode_Grab\n[5]--> Anode_Spacer\n[6]--> Cathode_Spacer\n[7]--> Cathode_Case")
+    os.chdir(os.path.dirname(__file__))
+    print("Choose from following list:\n[1]--> Anode\n[2]--> Anode_Grab\n[3]--> Cathode\n[4]--> Cathode_Grab\n[5]--> Anode_Spacer\n[6]--> Cathode_Spacer\n[7]--> Cathode_Case\n[8]--> Sucktion_Cap")
     object_id = input("-------------------------\nObject to test: ")
     try:
         object_id = int(object_id)
